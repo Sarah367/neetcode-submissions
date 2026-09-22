46f6409266b94d3b24cd331f0b80b532698b28a7
Sarah367 <komalagn2@csu.fullerton.edu>
2026-09-21 23:34:29 -0700

    Add: car-fleet - submission-8

---
+++ Data Structures & Algorithms/car-fleet/submission-8.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int carFleets = 0;
+        stack<double> st;
+
+        vector<pair<int,int>> car;
+
+        for (int i = 0; i < position.size(); i++) {
+            car.push_back({position[i], speed[i]});
+        }
+        sort(car.begin(), car.end());
+        
+
+        for (int i = car.size()-1; i >= 0; i--) {
+            double timeToTarget = double(target-car[i].first) / double(car[i].second);
+
+            if (!st.empty() && timeToTarget <= st.top()) {
+                continue;
+            } 
+            st.push(timeToTarget);
+        }
+
+        return st.size();
+
+    }
+};
