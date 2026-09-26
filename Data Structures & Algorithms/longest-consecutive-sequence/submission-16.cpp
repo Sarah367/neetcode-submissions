03ec65daf380057613d743b5065d0f7e674ca3f8
Sarah367 <komalagn2@csu.fullerton.edu>
2026-09-26 09:48:00 -0700

    Add: longest-consecutive-sequence - submission-16

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-16.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int,int> freq;
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        int cnt = 0, maxLength = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (freq.find(nums[i]-1) != freq.end()) {
+                continue;
+            }
+
+            int compare = nums[i];
+            while (freq.find(compare) != freq.end()) {
+                cnt++;
+                compare++;
+            }
+            maxLength = max(maxLength, cnt);
+            cnt = 0;
+        }
+
+
+        return maxLength;
+    }
+};
